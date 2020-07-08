2cb5f71b0d56e669d7f790be9932cf14cea8c2d7
George Hawkins <george-hawkins@users.noreply.github.com>
2020-07-08 19:07:15 +0200

    Dev environment `wsEndpoint` now always points to 'ws://ding-5cd80b3.local/socket' and cleared out much of the original data.service.ts code.

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
-//import { Observable, timer, Subject, EMPTY } from 'rxjs';
-//import { retryWhen, tap, delayWhen, switchAll, catchError } from 'rxjs/operators';
+import { Observable, timer, Subject, EMPTY } from 'rxjs';
+import { retryWhen, tap, delayWhen, switchAll, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
-//const RECONNECT_INTERVAL_MS = 2000;
+const RECONNECT_INTERVAL_MS = 2000;
 
 
 @Injectable({
@@ -17,49 +18,41 @@ export class DataService {
       return;
     }
 
-    this.socket$ = this.getNewWebSocket();
+    this.socket$ = this.createWebSocket();
 
-    // TODO: see https://rxjs-dev.firebaseapp.com/api/webSocket/webSocket
+    // See https://rxjs-dev.firebaseapp.com/api/webSocket/webSocket
+    // https://stackoverflow.com/a/44067972/245602 seem to suggest that retrying should be enough to trigger attempting to reconnect the underlying websocket.
+    // TODO: in which case I can get rid of the closeObserver below.
+    // Actually, it's very unclear what that's all about, if retryWhen on it's own doesn't reconnect, try the closeObserver route - note the weird
+    // `connect` parameter that causes `retryWhen` to be included if you call `connect(reconnect = true)` (as the close observer does) and not if you call
+    // just `connect()` as you do if your calling from AppComponet.
+    // You can probably also get rid of the `this.socket$ && !this.socket$.closed` guard above.
     // https://stackoverflow.com/a/23176223/245602
-    this.socket$.subscribe();
-
-    // const messages = this.socket$.pipe(
-    //   this.reconnect,
-    //   tap({
-    //     error: error => console.log(error),
-    //   }),
-    //   catchError(e => { 
-    //     console.log(e);
-    //     return EMPTY
-    //   })
-    // );
-  }
-
-  // private reconnect(o: Observable<any>): Observable<any> {
-  //   return o.pipe(
-  //     retryWhen(errors => errors.pipe(
-  //       tap(val => console.log('[Data Service] Try to reconnect', val)),
-  //       delayWhen(_ => timer(RECONNECT_INTERVAL_MS))
-  //     ))
-  //   );
-  // }
-
-  close() {
-    this.socket$.complete();
-    this.socket$ = undefined;
+    this.socket$.pipe(
+      retryWhen(errors =>
+        errors.pipe(
+          tap(e => console.log("Error", e)),
+          delayWhen(_ => timer(RECONNECT_INTERVAL_MS))
+        )
+      )
+    ).subscribe();
   }
 
   sendMessage(msg: any) {
     console.log(`attempting to send message ${msg}`);
 
+    // TODO: look at discarding messages if volume gets too high.
+
     this.socket$.next(msg);
   }
 
-  private getNewWebSocket(): WebSocketSubject<any> {
-    const endpoint = `ws://${location.host}/socket`;
+  private createWebSocket(): WebSocketSubject<any> {
+    // Amazingly, neither Typescript nor Javascript have some kind of String.format function.
+    const endpoint = environment.wsEndpoint.replace('{}', location.host);
 
     console.log(`attempting to connect to ${endpoint}`);
 
+    // TODO: if you don't set `serialize:` to something here then it defaults to JSON.stringify, hence double-quotes around strings.
     return webSocket({
       url: endpoint,
       openObserver: {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  wsEndpoint: 'ws://ding-5cd80b3.local/socket'
 };
 
 /*
