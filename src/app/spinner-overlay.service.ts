e0326a8d82d8cbe60fbe445326f2d765b575ecaf
George Hawkins <george-hawkins@users.noreply.github.com>
2020-07-10 18:27:03 +0200

    $ cp ../material-wifi-setup/src/app/spinner-overlay.service.ts src/app

---
+++ src/app/spinner-overlay.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Overlay, OverlayRef, OverlayConfig } from '@angular/cdk/overlay';
+import { ComponentPortal } from '@angular/cdk/portal';
+import { MatSpinner } from '@angular/material/progress-spinner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpinnerOverlayService {
+  private overlayRef: OverlayRef;
+  private portal = new ComponentPortal(MatSpinner);
+
+  constructor(overlay: Overlay) {
+    this.overlayRef = overlay.create(this.config(overlay));
+  }
+
+  public show() {
+    if (!this.overlayRef.hasAttached()) {
+      this.overlayRef.attach(this.portal);
+    }
+  }
+
+  public hide() {
+    if (this.overlayRef.hasAttached()) {
+      this.overlayRef.detach();
+    }
+  }
+
+  private config(overlay: Overlay): OverlayConfig {
+    // Note: scrollStrategies.block() only works if the backdrop is already scollable.
+    // See https://github.com/angular/components/issues/19020
+
+    return {
+      hasBackdrop: true,
+      scrollStrategy: overlay.scrollStrategies.block(),
+      positionStrategy: overlay.position()
+        .global()
+        .centerHorizontally()
+        .centerVertically()
+    };
+  }
+}
